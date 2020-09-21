326f7517d90c8cc8bea42fca2ac52c9089fa83cf
Rebeca Ruiz <contacto@rebecaruiz.com>
2020-09-21 12:46:22 +0200

    improved wording

--- js/ex80.js
+++ js/ex80.js
@@ -4,11 +4,11 @@ la 2ª a la 4ª posición en una cadena dada. */
 
 const pangrama = "El veloz murciélago hindú comía feliz cardillo y kiwi. La cigüeña tocaba el saxofón detrás del palenque de paja.";
 
-const respuesta = prompt("Escribe una letra cualquiera");
+const respuesta = prompt("Escribe un carácter cualquiera y comprueba si está entre la segunda y cuarta posición de nuestra frase secreta.");
 
 alert (`Has escogido ${respuesta}`);
 
 const posicion = pangrama.slice(1,4).includes(respuesta);
 
-alert(posicion?'Encontrado':'No encontrado');
+alert(posicion?'Carácter encontrado':'Carácter no encontrado');
 
